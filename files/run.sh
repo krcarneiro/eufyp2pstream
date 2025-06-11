77aad5badfdf2661a32631fcc5341847c8c3d05f
krcarneiro <krcarneiro@gmail.com>
2025-06-11 13:12:24 -0300

    Update run.sh

--- files/run.sh
+++ files/run.sh
@@ -3,7 +3,8 @@ set +u
 CONFIG_PATH=/data/options.json
 
 EUFY_WS_PORT=$(jq --raw-output ".eufy_security_ws_port" $CONFIG_PATH)
+EUFY_DEVICE_SERIAL=$(jq --raw-output ".eufy_device_serial" $CONFIG_PATH)
 
-echo "Starting EufyP2PStream. eufy_security_ws_port is $EUFY_WS_PORT"
-python3 -u /eufyp2pstream.py $EUFY_WS_PORT
+echo "Starting EufyP2PStream. eufy_security_ws_port is $EUFY_WS_PORT eufy_security_ws_serialnumer is $EUFY_DEVICE_SERIAL"
+python3 -u /eufyp2pstream.py $EUFY_WS_PORT $EUFY_DEVICE_SERIAL
 echo "Exited with code $?"
